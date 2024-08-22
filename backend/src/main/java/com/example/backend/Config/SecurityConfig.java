dc4cb2b665f24df3ed83e877bb07e4657ea033d9
hanseongeon <gkstjddjs08@naver.com>
2024-08-22 21:59:51 +0900

    start

---
+++ backend/src/main/java/com/example/backend/Config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.backend.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests ((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers ("/user/**", "/ttt").permitAll ().anyRequest ().authenticated ())
+                .formLogin (formLogin ->
+                        formLogin.loginPage ("/user/login")
+                                .defaultSuccessUrl ("/", true)
+                )
+                .sessionManagement(session -> session
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false)
+                )
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
+                .csrf(c -> c.ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/**")
+                ));
+        return http.build ();
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager ();
+    }
+}
---
+++ backend/src/main/java/com/example/backend/Entity/SiteUser.java
@@ -0,0 +1,23 @@
+package com.example.backend.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class SiteUser {
+
+    @Id
+    private String id;
+
+    private String nickName;
+
+    private String password;
+
+
+}
